32ef023617fa97f799d2ac16a44d8455c860d681
charlenezell <charlenezell@gmail.com>
2020-01-05 13:58:12 +0800

    push build

---
+++ dist/precache-manifest.e4bc413b540b793b74ef806f74ecd27c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae860ffcb2a39546fc7",
+    "url": "/dist/css/app.2c8edd6e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/dist/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7635cb3344a921a4bd75ad591b5fa9c4",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "444bd99864f26e7a760d",
+    "url": "/dist/js/about.8353ef6d.js"
+  },
+  {
+    "revision": "7ae860ffcb2a39546fc7",
+    "url": "/dist/js/app.77f7d1bf.js"
+  },
+  {
+    "revision": "711baf2c8741138b4cf3",
+    "url": "/dist/js/chunk-vendors.940ec106.js"
+  },
+  {
+    "revision": "ba69350103bf695d475afd977a69252f",
+    "url": "/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dist/robots.txt"
+  }
+]);
